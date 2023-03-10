49b6a6b1196d296cdee84036c8f0ee6c9121d572
heibaiyuanfen <106620912+heibaiyuanfen@users.noreply.github.com>
2023-03-10 11:34:25 +0800

    Add files via upload
    最终修改的mobilenet模板

--- common.py
+++ common.py
@@ -20,6 +20,7 @@ import numpy as np
 import pandas as pd
 import requests
 import torch
+import torchvision.models as models
 import torch.nn as nn
 from PIL import Image
 from torch.cuda import amp
@@ -42,16 +43,23 @@ def autopad(k, p=None, d=1):  # kernel, padding, dilation
     return p
 
 
-class MobileNet(nn.Module):
-    def __init__(self, *args) -> None:
+# Mobilenetv3Small
+class SeBlock(nn.Module):
+    def __init__(self, in_channel, reduction=4):
         super().__init__()
-        model = models.mobilenet_v3_small(pretrained=True)
-        modules = list(model.children())
-        modules = modules[0][args[1]:args[2]]
-        self.model = nn.Sequential(*modules)
+        self.Squeeze = nn.AdaptiveAvgPool2d(1)
+
+        self.Excitation = nn.Sequential()
+        self.Excitation.add_module('FC1', nn.Conv2d(in_channel, in_channel // reduction, kernel_size=1))  # 1*1卷积与此效果相同
+        self.Excitation.add_module('ReLU', nn.ReLU())
+        self.Excitation.add_module('FC2', nn.Conv2d(in_channel // reduction, in_channel, kernel_size=1))
+        self.Excitation.add_module('Sigmoid', nn.Sigmoid())
 
     def forward(self, x):
-        return self.model(x)
+        y = self.Squeeze(x)
+        ouput = self.Excitation(y)
+        return x * (ouput.expand_as(x))
+    
 
 
 class Conv(nn.Module):
@@ -70,6 +78,70 @@ class Conv(nn.Module):
     def forward_fuse(self, x):
         return self.act(self.conv(x))
 
+class Conv_BN_HSwish(nn.Module):
+    """
+    This equals to
+    def conv_3x3_bn(inp, oup, stride):
+        return nn.Sequential(
+            nn.Conv2d(inp, oup, 3, stride, 1, bias=False),
+            nn.BatchNorm2d(oup),
+            h_swish()
+        )
+    """
+
+    def __init__(self, c1, c2, stride):
+        super(Conv_BN_HSwish, self).__init__()
+        self.conv = nn.Conv2d(c1, c2, 3, stride, 1, bias=False)
+        self.bn = nn.BatchNorm2d(c2)
+        self.act = nn.Hardswish()
+
+    def forward(self, x):
+        return self.act(self.bn(self.conv(x)))
+
+class MobileNetV3_InvertedResidual(nn.Module):
+    def __init__(self, inp, oup, hidden_dim, kernel_size, stride, use_se, use_hs):
+        super(MobileNetV3_InvertedResidual, self).__init__()
+        assert stride in [1, 2]
+
+        self.identity = stride == 1 and inp == oup
+
+        if inp == hidden_dim:
+            self.conv = nn.Sequential(
+                # dw
+                nn.Conv2d(hidden_dim, hidden_dim, kernel_size, stride, (kernel_size - 1) // 2, groups=hidden_dim,
+                          bias=False),
+                nn.BatchNorm2d(hidden_dim),
+                nn.Hardswish() if use_hs else nn.ReLU(),
+                # Squeeze-and-Excite
+                SeBlock(hidden_dim) if use_se else nn.Sequential(),
+                # pw-linear
+                nn.Conv2d(hidden_dim, oup, 1, 1, 0, bias=False),
+                nn.BatchNorm2d(oup),
+            )
+        else:
+            self.conv = nn.Sequential(
+                # pw
+                nn.Conv2d(inp, hidden_dim, 1, 1, 0, bias=False),
+                nn.BatchNorm2d(hidden_dim),
+                nn.Hardswish() if use_hs else nn.ReLU(),
+                # dw
+                nn.Conv2d(hidden_dim, hidden_dim, kernel_size, stride, (kernel_size - 1) // 2, groups=hidden_dim,
+                          bias=False),
+                nn.BatchNorm2d(hidden_dim),
+                # Squeeze-and-Excite
+                SeBlock(hidden_dim) if use_se else nn.Sequential(),
+                nn.Hardswish() if use_hs else nn.ReLU(),
+                # pw-linear
+                nn.Conv2d(hidden_dim, oup, 1, 1, 0, bias=False),
+                nn.BatchNorm2d(oup),
+            )
+
+    def forward(self, x):
+        y = self.conv(x)
+        if self.identity:
+            return x + y
+        else:
+            return y
 
 class DWConv(Conv):
     # Depth-wise convolution
